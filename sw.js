0906582dfc49d86d46179506e4e2959dedb5e48e
hoyeonkim <hy940313@gmail.com>
2021-02-02 18:06:34 +0900

    console 로그 찾기

--- sw.js
+++ sw.js
@@ -33,11 +33,11 @@ self.addEventListener('install', (e) => {
 self.addEventListener('fetch', (e) => {
   e.respondWith(
     caches.match(e.request).then((r) => {
-      console.log('[Service Worker] Fetching resource: ' + e.request.url);
+      /* console.log('[Service Worker] Fetching resource: ' + e.request.url); */
       return r || fetch(e.request).then((response) => {
         return caches.open(cacheName).then((cache) => {
           if (!isExcluded(e.request.url)) {
-            /* console.log('[Service Worker] Caching new resource: ' + e.request.url); */
+            console.log('[Service Worker] Caching new resource: ' + e.request.url);
             cache.put(e.request, response.clone());
           }
           return response;
