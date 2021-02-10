34c4145b8b94285ba5ed135c19aeea0f11a3e9d6
hoyeonkim <hy940313@gmail.com>
2021-02-11 00:23:18 +0900

    delete sw.js 두번째 함수

--- sw.js
+++ sw.js
@@ -30,23 +30,6 @@ self.addEventListener('install', (e) => {
 });
 
 
-self.addEventListener('fetch', (e) => {
-  e.respondWith(
-    caches.match(e.request).then((r) => {
-      /* console.log('[Service Worker] Fetching resource: ' + e.request.url); */
-      return r || fetch(e.request).then((response) => {
-        return caches.open(cacheName).then((cache) => {
-          if (!isExcluded(e.request.url)) {
-            /* console.log('[Service Worker] Caching new resource: ' + e.request.url); */
-            cache.put(e.request, response.clone());
-          }
-          return response;
-        });
-      });
-    })
-  );
-});
-
 
 self.addEventListener('activate', (e) => {
   e.waitUntil(
