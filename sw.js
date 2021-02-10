d7ac9d09f38c093d6ed6860784453eb9556d0c65
hoyeonkim <hy940313@gmail.com>
2021-02-11 00:30:08 +0900

    push -> add

--- sw.js
+++ sw.js
@@ -30,6 +30,23 @@ self.addEventListener('install', (e) => {
 });
 
 
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((r) => {
+      /* console.log('[Service Worker] Fetching resource: ' + e.request.url); */
+      return r || fetch(e.request).then((response) => {
+        return caches.open(cacheName).then((cache) => {
+          if (!isExcluded(e.request.url)) {
+            /* console.log('[Service Worker] Caching new resource: ' + e.request.url); */
+            cache.add(e.request, response.clone());
+          }
+          return response;
+        });
+      });
+    })
+  );
+});
+
 
 self.addEventListener('activate', (e) => {
   e.waitUntil(
