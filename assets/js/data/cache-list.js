20ca5b1270f69e33d2d19f50968f830f45c7bdc1
hoyeonkim <hy940313@gmail.com>
2021-04-03 01:17:42 +0900

    roll back

--- assets/js/data/cache-list.js
+++ assets/js/data/cache-list.js
@@ -2,31 +2,41 @@
 layout: compress
 
 # The list to be cached by PWA
+# Chirpy v2.2
+# https://github.com/cotes2020/jekyll-theme-chirpy
+# © 2020 Cotes Chung
+# MIT Licensed
 ---
 
 const include = [
-  /* --- CSS --- */
 
-  '{{ "/assets/css/style.css" | relative_url }}',
+  /*--- CSS ---*/
 
-  /* --- Javascripts --- */
-  '{{ "/assets/js/dist/home.min.js" | relative_url }}',
-  '{{ "/assets/js/dist/page.min.js" | relative_url }}',
-  '{{ "/assets/js/dist/post.min.js" | relative_url }}',
-  '{{ "/assets/js/dist/categories.min.js" | relative_url }}',
-  '{{ "/assets/js/data/search.json" | relative_url }}',
-  '{{ "/app.js" | relative_url }}',
-  '{{ "/sw.js" | relative_url }}',
+  '{{ "/assets/css/home.css" | relative_url }}',
+  '{{ "/assets/css/categories.css" | relative_url }}',
+  '{{ "/assets/css/tags.css" | relative_url }}',
+  '{{ "/assets/css/archives.css" | relative_url }}',
+  '{{ "/assets/css/page.css" | relative_url }}',
+  '{{ "/assets/css/post.css" | relative_url }}',
+  '{{ "/assets/css/category-tag.css" | relative_url }}',
+  '{{ "/assets/css/lib/bootstrap-toc.min.css" | relative_url }}',
 
-  /* --- HTML --- */
-  '{{ "/index.html" | relative_url }}',
-  '{{ "/404.html" | relative_url }}',
+  /*--- Javascripts ---*/
+
+  '{{ "/assets/js/home.min.js" | relative_url }}',
+  '{{ "/assets/js/page.min.js" | relative_url }}',
+  '{{ "/assets/js/post.min.js" | relative_url }}',
+  '{{ "/assets/js/categories.min.js" | relative_url }}',
+
+
+  /*--- HTML ---*/
+
+  /* Tabs */
   {% for tab in site.tabs %}
     '{{ tab.url }}',
   {% endfor %}
 
-
-  /* --- Icons --- */
+  /*--- Icons ---*/
 
   {%- capture icon_url -%}
     {{ "/assets/img/favicons" | relative_url }}
@@ -49,7 +59,14 @@ const include = [
   '{{ icon_url }}/favicon-16x16.png',
   '{{ icon_url }}/ms-icon-144x144.png',
   '{{ icon_url }}/manifest.json',
-  '{{ icon_url }}/browserconfig.xml'
+  '{{ icon_url }}/browserconfig.xml',
+
+  /*--- Others ---*/
+
+  '{{ "/assets/js/data/search.json" | relative_url }}',
+  '{{ "/404.html" | relative_url }}',
+  '{{ "/app.js" | relative_url }}',
+  '{{ "/sw.js" | relative_url }}'
 ];
 
 const exclude = [
@@ -57,5 +74,5 @@ const exclude = [
     '{{ site.google_analytics.pv.proxy_url }}',
   {%- endif -%}
   '/assets/js/data/pageviews.json',
-  '/img.shields.io/'
+  '/img.shields.io/',
 ];
--- sw.js
+++ sw.js
@@ -7,6 +7,7 @@ self.importScripts('{{ "/assets/js/data/cache-list.js" | relative_url }}');
 
 var cacheName = 'chirpy-{{ "now" | date: "%Y%m%d.%H%M" }}';
 
+
 function isExcluded(url) {
   const regex = /(^http(s)?|^\/)/; /* the regex for CORS url or relative url */
   for (const rule of exclude) {
@@ -18,6 +19,7 @@ function isExcluded(url) {
   return false;
 }
 
+
 self.addEventListener('install', (e) => {
   self.skipWaiting();
   e.waitUntil(
@@ -27,25 +29,23 @@ self.addEventListener('install', (e) => {
   );
 });
 
+
 self.addEventListener('fetch', (e) => {
   e.respondWith(
     caches.match(e.request).then((r) => {
-      /* console.log(`[sw] method: ${e.request.method}, fetching: ${e.request.url}`); */
+      /* console.log('[Service Worker] Fetching resource: ' + e.request.url); */
       return r || fetch(e.request).then((response) => {
         return caches.open(cacheName).then((cache) => {
           if (!isExcluded(e.request.url)) {
-            if (e.request.method === "GET") {
-              /* console.log('[sw] Caching new resource: ' + e.request.url); */
-              cache.put(e.request, response.clone());
-            }
+            /* console.log('[Service Worker] Caching new resource: ' + e.request.url); */
           }
           return response;
         });
-
       });
     })
   );
 });
+
 
 self.addEventListener('activate', (e) => {
   e.waitUntil(
